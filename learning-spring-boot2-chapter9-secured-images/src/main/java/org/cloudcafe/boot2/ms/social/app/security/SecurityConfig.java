828bafe6e59f38c144ac47858cf5e74701ee7416
Ravindra Gadiparthi <Ravindra.Gadiparthi@vcontractor.co.za>
2019-12-27 07:22:21 +0530

    updating hystrix configuration

--- learning-spring-boot2-chapter9-secured-images/src/main/java/org/cloudcafe/boot2/ms/social/app/security/SecurityConfig.java
+++ learning-spring-boot2-chapter9-secured-images/src/main/java/org/cloudcafe/boot2/ms/social/app/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.cloudcafe.boot2.ms.social.app.security;
 
+import org.cloudcafe.boot2.ms.social.app.filter.UserContextFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
@@ -7,10 +9,14 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 
+import static org.springframework.security.config.web.server.SecurityWebFiltersOrder.LAST;
+
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    UserContextFilter contextFilter;
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain() {
@@ -18,6 +24,7 @@ public class SecurityConfig {
                 .authorizeExchange().anyExchange().authenticated()
                 .and()
                 .securityContextRepository(new WebSessionServerSecurityContextRepository())
+                .addFilterAfter(contextFilter, LAST)
                 .cors().disable()
                 .csrf().disable()
                 .build();
